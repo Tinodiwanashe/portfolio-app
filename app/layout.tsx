47be4c90c1b62c385a77fb31d24cd2db1a945869
Munyaradzi K <kandoromt1@outlook.com>
2024-07-31 10:39:50 +0200

    configured dark mode on cleark provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,14 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Head from "next/head";
 import { RootLayoutProps } from "./types/definitions";
 import { Metadata } from 'next'
-import { ClerkProvider, Theme  } from "@clerk/nextjs";
+import { ClerkProvider  } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
+import { dark } from '@clerk/themes';
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
-
-// Define the dark theme
-const dark: Theme = {
-  baseTheme: "dark"
-  // Add other properties as needed
-};
 
 export const metadata: Metadata  = {
   title: "Munyaradzi Kandoro Portfolio",
@@ -24,8 +20,17 @@ export const metadata: Metadata  = {
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const { theme } = useTheme();
+
+  const clerkAppearance = {
+    baseTheme: theme === "dark" ? dark : undefined,
+    elements: {
+      formButtonPrimary:
+        "bg-primary text-primary-foreground shadow hover:bg-primary/90 text-sm font-medium",
+    },
+  }
   return (
-    <ClerkProvider appearance={dark}>
+    <ClerkProvider appearance={clerkAppearance}>
       <html lang="en" suppressHydrationWarning>
         <Head>
           <link rel="icon" href="/favicon.ico" sizes="any" />
