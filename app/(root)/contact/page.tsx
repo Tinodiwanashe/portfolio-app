6a01d79bcae1b1e6f1865cde4db160a628fa2579
Munyaradzi K <kandoromt1@outlook.com>
2024-08-07 17:27:02 +0200

    contact form update 2

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -7,7 +7,9 @@ import React from 'react'
 
 const page = () => {
 
-  const contact = () => {
+  const contact = async () => {
+    "use server";
+
     alert('Contact form submitted')
   }
 
@@ -41,13 +43,13 @@ const page = () => {
 
   return (
     <main className="py-14">
-      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+      <div className="max-w-screen-xl mx-auto px-4 text-neutral-50 md:px-8">
           <div className="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none">
               <div className="max-w-lg space-y-3">
                   <h3 className="text-indigo-600 font-semibold">
                     Contact
                   </h3>
-                  <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                  <p className="text-neutral-50 text-3xl font-semibold sm:text-4xl">
                     Let us know how we can help
                   </p>
                   <p>
@@ -77,7 +79,7 @@ const page = () => {
                           <Input
                             type="text"
                             required
-                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                            className="w-full mt-2 px-3 py-2 text-neutral-50 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                           />
                       </div>
                       <div>
