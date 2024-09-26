56aea85200af7f0dfba70650fa912e7f0475731d
Munyaradzi K <kandoromt1@outlook.com>
2024-09-26 13:29:55 +0200

    fixed: Unexpected token `DropdownMenu`. Expected jsx identifier

--- app/(root)/users/[userId]/_components/TableOptions.tsx
+++ app/(root)/users/[userId]/_components/TableOptions.tsx
@@ -1,37 +1,39 @@
-import { Button } from '@/components/ui/button'
-import { api } from '@/convex/_generated/api'
-import { Id } from '@/convex/_generated/dataModel'
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from '@/components/ui/dropdown-menu'
-import { useMutation } from 'convex/react'
-import { revalidatePath } from 'next/cache'
-import Link from 'next/link'
-import { redirect } from 'next/navigation'
-import React from 'react'
-import { FaTrash } from 'react-icons/fa'
-import { FaEllipsisVertical, FaPencil } from 'react-icons/fa6'
+import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
+import { useMutation } from 'convex/react';
+import { revalidatePath } from 'next/cache';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import React from 'react';
+import { FaTrash } from 'react-icons/fa';
+import { FaEllipsisVertical, FaPencil } from 'react-icons/fa6';
 
 export default async function TableOptions({id}: {id: Id<"User"> }) {
   return (
-    <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-            <Button
-                aria-haspopup="true"
-                size="icon"
-                variant="ghost"
-            >
-                <FaEllipsisVertical className="h-4 w-4" />
-                <span className="sr-only">Toggle menu</span>
-            </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <EditUser id={id}/>
-        <DeleteUser id={id}>
-        </DropdownMenuContent>
-    </DropdownMenu>
-  )
-}
+    <>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    aria-haspopup="true"
+                    size="icon"
+                    variant="ghost"
+                >
+                    <FaEllipsisVertical className="h-4 w-4" />
+                    <span className="sr-only">Toggle menu</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <EditUser id={id}/>
+            <DeleteUser id={id}/>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </>
+  );
+};
 
 const EditUser = ({ id }: { id: Id<"User"> }) => {
     function redirectToUserForm(){
