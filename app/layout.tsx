c114d3166e7206174d8dec4334f38c86e8faa510
Munyaradzi K <kandoromt1@outlook.com>
2024-09-27 17:25:47 +0200

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter as FontSans } from "next/font/google"
 import "./styles/globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
-import Head from "next/head";
 import { RootLayoutProps } from "./types/definitions";
 import { Metadata } from 'next';
 import { ConvexClientProvider } from "@/components/ui/ConvexClientProvider";
@@ -16,37 +15,22 @@ const fontSans = FontSans({
 })
 
 export const metadata: Metadata  = {
-  title: "Munyaradzi Kandoro Portfolio",
-  description: "A Tech Enthusiast Portfolio Website"
+  title: {
+    template: '%s | Portfolio',
+    default: 'Munyaradzi Kandoro Portfolio'
+  },
+  description: "A Tech Enthusiast Portfolio Website",
+  //metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  icons: {
+    icon: '/favicon.ico'
+  }
 };
+//The %s in the template will be replaced with the specific page title. -----Is redirect working
 
 export default function RootLayout({ children }: RootLayoutProps) {
-/*   async function getTheme() {
-    'use server'
-    const {theme} = useTheme();
-    return theme;
-    // ...
-  } */
-  
- 
-/*   useEffect(() => {
-    const fetchTheme = async () => {
-      setNextTheme(useTheme());
-    }
- 
-    fetchTheme().catch((e) => {
-      // handle the error as needed
-      console.error('An error occurred while fetching the data: ', e)
-    })
-  }, [])
- */
-
 
   return (  
       <html lang="en" suppressHydrationWarning>
-        <Head>
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-        </Head>
         <body className={cn("min-h-screen w-full flex flex-col bg-background text-foreground font-sans antialiased",fontSans.variable)}>
           <Toaster />
           <ConvexClientProvider>
