b8630845ddcdf9c5c6f0cc432d581f9fc8e74ecf
Munyaradzi K <kandoromt1@outlook.com>
2024-11-25 17:48:47 +0200

    one massive fix 3

--- app/(root)/settings/skills/_components/NewSkillLinkForm.tsx
+++ app/(root)/settings/skills/_components/NewSkillLinkForm.tsx
@@ -23,7 +23,7 @@ type PreloadedProps = {
 
 export default function NewSkillLinkForm(props: PreloadedProps) {  
   const skills = useQuery(api.skills.getOtherSkills,{id: props.parentId});  
-  const parentSkill = useQuery(api.skills.getSkill,{SkillId: props.parentId});  
+  const parentSkill = useQuery(api.skills.getSkill,{id: props.parentId});  
   const childSkills = useQuery(api.skillLinks.getChildSkills,{parentId: props.parentId}); 
   const {
     mutate,
